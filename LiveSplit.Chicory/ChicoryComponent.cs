5a5104aab4184096c06b87dce575ac3a7e844f52
grimelios <grimelios@gmail.com>
2021-06-29 00:11:22 -0500

    Moving files

---
+++ LiveSplit.Chicory/ChicoryComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiveSplit.Autosplit;
+
+namespace LiveSplit.Chicory
+{
+	public class ChicoryComponent : AutosplitComponent
+	{
+	}
+}
---
+++ LiveSplit.Chicory/ChicoryFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiveSplit.Autosplit;
+using LiveSplit.Model;
+using LiveSplit.UI.Components;
+
+namespace LiveSplit.Chicory
+{
+	public class ChicoryFactory : AutosplitFactory
+	{
+		public override IComponent Create(LiveSplitState state)
+		{
+			return new ChicoryComponent();
+		}
+	}
+}
